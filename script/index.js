d968aec9eaee562d20b16d4ffe8ca59c713b16e2
EduardoRodriguez <rodriguezalcuadrado@proton.me>
2022-07-29 17:48:33 -0400

    formate text

--- script/index.js
+++ script/index.js
@@ -1,4 +1,3 @@
-
 /*-------- Profile Form --------*/
 const form = document.querySelector(".form");
 const formBtnClose = form.querySelector(".form__btn-close");
@@ -6,23 +5,23 @@ const formBtnSubmit = form.querySelector(".form__btn-submit");
 
 const btnEditProfile = document.querySelector(".btn-edit");
 
-formBtnClose.addEventListener("click", function(){
-    form.classList.toggle("form_hidden");
+formBtnClose.addEventListener("click", function () {
+  form.classList.toggle("form_hidden");
 });
 
-formBtnSubmit.addEventListener("click", function(){
-    const profileName = document.querySelector(".profile__name");
-    const profileAbout = document.querySelector(".profile__about");
+formBtnSubmit.addEventListener("click", function () {
+  const profileName = document.querySelector(".profile__name");
+  const profileAbout = document.querySelector(".profile__about");
 
-    const inputName = document.querySelector(".form__name");
-    const inputAbout = document.querySelector(".form__about");
+  const inputName = document.querySelector(".form__name");
+  const inputAbout = document.querySelector(".form__about");
 
-    profileName.textContent = inputName.value;
-    profileAbout.textContent = inputAbout.value;
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
 
-    form.classList.toggle("form_hidden");
+  form.classList.toggle("form_hidden");
 });
 
-btnEditProfile.addEventListener("click", function(){
-    form.classList.toggle("form_hidden");
+btnEditProfile.addEventListener("click", function () {
+  form.classList.toggle("form_hidden");
 });
